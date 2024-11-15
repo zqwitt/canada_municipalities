cc130ac81d2392bb11df12bce31ea09271b9176c
Zachary Witt <zach.witt95@gmail.com>
2024-11-15 13:43:30 -0500

    Update municipalities.ts

--- municipalities.ts
+++ municipalities.ts
@@ -1,4 +1,51 @@
-export const municipalities = [
+export interface Municipality {
+  name: string;
+  id: {
+    dguid: string; // See https://www150.statcan.gc.ca/n1/pub/92f0138m/92f0138m2019001-eng.htm for details
+    vintage: string;
+    type: "A";
+    schema: "2021";
+    geouid: string; // 7 digit Geographic Unique Identifier
+    pruid: string; // 2 digit province code
+    cduid: string; // 2 digit census division code
+    csduid: string; // 3 digit census subdivision code
+  };
+  province: {
+    abbr: "AB" | "BC" | "MB" | "NB" | "NL" | "NS" | "ON" | "PE" | "QC" | "SK" | "NT" | "NU" | "YT";
+    en:
+      | "Alberta"
+      | "British Columbia"
+      | "Manitoba"
+      | "New Brunswick"
+      | "Newfoundland and Labrador"
+      | "Nova Scotia"
+      | "Ontario"
+      | "Prince Edward Island"
+      | "Quebec"
+      | "Saskatchewan"
+      | "Northwest Territories"
+      | "Nunavut"
+      | "Yukon";
+    fr:
+      | "Alberta"
+      | "Colombie-Britannique"
+      | "Manitoba"
+      | "Nouveau-Brunswick"
+      | "Terre-Neuve-et-Labrador"
+      | "Nouvelle-Écosse"
+      | "Ontario"
+      | "Île-du-Prince-Édouard"
+      | "Québec"
+      | "Saskatchewan"
+      | "Territoires du Nord-Ouest"
+      | "Nunavut"
+      | "Yukon";
+  };
+}
+
+type Municipalities = Array<Municipality>
+
+export const municipalities: Municipalities =  [
   {
     name: "Division No.  1, Subd. V",
     id: {
